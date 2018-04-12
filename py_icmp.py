39564b8e44a7d71603c6bc5bd7052bf3831d3b11
Hans <s9528244@gmail.com>
2018-04-12 11:23:17 +0800

    Socket_python
    This is a self-entertainment project on python socket programming.

---
+++ py_icmp.py
@@ -0,0 +1,6 @@
+#This is a gadget for sending icmp
+# 1. Send ICMP request
+# 2. Recv ICMP response
+# 3. configure the send interval
+# 4. Configure the timeout value
+# 5. Record the result on CSV file.
