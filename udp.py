32f9e2118b2055022e61238d20c117c7e81d2e2c
Handauru <s9528244@gmail.com>
2018-05-07 18:06:56 +0800

    2018-05-07
    Add UDP file

---
+++ udp.py
@@ -0,0 +1,10 @@
+#This is a gadget for sending UDP
+# 1. Send UDP
+# 2. configure the data size
+# 3. configure the send interval
+# 4. configure the dst addr
+# 4.1 Unicast
+# 4.2 Broadcast
+# 4.3 Multicast
+
+
