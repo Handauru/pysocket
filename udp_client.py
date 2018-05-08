64d519772e9797a37c499043ceef9a9c5112d881
Handauru <s9528244@gmail.com>
2018-05-08 22:40:36 +0800

    2018-05-08
    add UDP-Server & Client

---
+++ udp_client.py
@@ -0,0 +1,24 @@
+#This is a gadget for sending UDP
+# 1. Send UDP
+# 2. configure the data size
+# 3. configure the send interval
+# 4. configure the dst addr
+# 4.1 Unicast
+# 4.2 Broadcast
+# 4.3 Multicast
+#-------------------------------
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Originated from https://github.com/michaelliao/learn-python3/blob/master/samples/socket/udp_client.py
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+for data in [b'Michael', b'Tracy', b'Sarah']:
+    # 发送数据:
+    s.sendto(data, ('127.0.0.1', 9999))
+    # 接收数据:
+    print(s.recv(1024).decode('utf-8'))
+
+s.close()
---
+++ udp_server.py
@@ -0,0 +1,29 @@
+#This is a gadget for sending UDP
+# 1. Send UDP
+# 2. configure the data size
+# 3. configure the send interval
+# 4. configure the dst addr
+# 4.1 Unicast
+# 4.2 Broadcast
+# 4.3 Multicast
+#---------------------------------
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Originated from https://github.com/michaelliao/learn-python3/blob/master/samples/socket/udp_server.py
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# 绑定端口:
+s.bind(('127.0.0.1', 9999))
+
+print('Bind UDP on 9999...')
+
+while True:
+    # 接收数据:
+    data, addr = s.recvfrom(1024)
+    print('Received from %s:%s.' % addr)
+    reply = 'Hello, %s!' % data.decode('utf-8')
+    s.sendto(reply.encode('utf-8'), addr)
+
